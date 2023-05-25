f8b5b1998b51161ae61f731514ff852f56b0f2d0
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-25 11:15:32 -0700

    add loginPage functionality with authentication

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -5,56 +5,61 @@ import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
 import BackgroundImage from "../components/BackgroundImage";
 import styled from "styled-components";
+import { firebaseAuth } from "../utils/firebase-config";
 
 
 
 const LoginPage = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
 
-    const navigate = useNavigate()
+  const navigate = useNavigate()
 
-    const handleLogin = async () => {
-        try {
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-        } catch (error) {
-            console.log(error);
-        }
-    };
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) navigate("/");
+  });
 
 
 
-    return (
-        <Wrapper>
-            <BackgroundImage />
-            <div className="loginContent">
-                <Header />
-                <div className="form-wrapper">
-                    <div className="form">
-                        <div className="title">
-                            <h1>login</h1>
-                        </div>
-                        <div className="container">
-                            <input
-                                type="text"
-                                placeholder="Email"
-                                onChange={(e) => setEmail(e.target.value)}
-                                value={email}
-                            />
-                            <input
-                                type="password"
-                                placeholder="Password"
-                                onChange={(e) => setPassword(e.target.value)}
-                                value={password}
-                            />
-                            <button onClick={handleLogin}>Login</button>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <Wrapper>
+      <BackgroundImage />
+      <div className="loginContent">
+        <Header />
+        <div className="form-wrapper">
+          <div className="form">
+            <div className="title">
+              <h1>login</h1>
             </div>
-        </Wrapper>
-    );
+            <div className="container">
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+              <button onClick={handleLogin}>Login</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.div`
   position: relative;
