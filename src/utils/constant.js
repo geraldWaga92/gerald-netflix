6da5ade5fee8547a541cbb5dadce8ca20a51e743
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-25 10:57:08 -0700

    update constant

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,2 +1,1 @@
-export const MY_API_KEY = "AIzaSyCvnWn5p2c02YlECurD0OWLYFki4bXIxA0";
-export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
+export const TMDB_BASE_URL = "https://api.themoviedb.org/3"
