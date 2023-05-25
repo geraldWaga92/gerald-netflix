25ef72c2f03c5acc5128649253994fd84d52959b
Gerald Waga <106008160+geraldWaga92@users.noreply.github.com>
2023-05-25 09:51:15 -0700

    update app with route component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignupPage";
+import Player from "./pages/Player";
+import TvShow from "./pages/TvShow";
+import Netflix from "./pages/Netflix";
+import MoviePage from "./pages/MoviePage";
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/login" element={<LoginPage />} />
+        <Route exact path="/signup" element={<SignUpPage />} />
+        <Route exact path="/player" element={<Player />} />
+        <Route exact path="/tv" element={<TvShow />} />
+        <Route exact path="/" element={<Netflix />} />
+        <Route exact path="/movie" element={<MoviePage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
